cfc588800f9a20cecdcbc27a0ab4f9ddf192888b
Umer Farooq <umerfarooq@Umers-MacBook-Air.local>
2024-03-28 21:44:23 +0100

    contact form changes

--- assets/mail/contact2.php
+++ assets/mail/contact2.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../../vendor/phpmailer/src/Exception.php';
+require '../../vendor/phpmailer/src/PHPMailer.php';
+require '../../vendor/phpmailer/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Process the POST request
@@ -16,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $homeland = $_POST['country'];
     $company_type = $_POST['choice_company'];
     $services = implode(", ", $_POST['services']);
-    $message = $_POST['message'];
+    $message_body = $_POST['message'];
     $budget = isset($_POST['budget']) ? $_POST['budget'] : 'Not specified';
     $privacy_policy_accept = isset($_POST['privacy_policy_accept']) ? $_POST['privacy_policy_accept'] : '0';
 
@@ -44,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } elseif (empty($services)) {
         echo '<div class="alert alert-error">Please select the services you need!</div>';
         exit();
-    } elseif (empty($message)) {
+    } elseif (empty($message_body)) {
         echo '<div class="alert alert-error">You must enter your message.</div>';
         exit();
     } elseif (!isset($privacy_policy_accept) || $privacy_policy_accept != '1') {
@@ -52,30 +58,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
-    $to = "wordpressriver@gmail.com";
-    $subject = "Contact Form Submission from $fname $lname";
-    $message = "You have received a contact form submission from $fname $lname. Below are the details:" . PHP_EOL . PHP_EOL;
-    $message .= "First Name: $fname" . PHP_EOL;
-    $message .= "Last Name: $lname" . PHP_EOL;
-    $message .= "Email: $email" . PHP_EOL;
-    $message .= "Phone Number: $phone" . PHP_EOL;
-    $message .= "Country: $homeland" . PHP_EOL;
-    $message .= "Company Type: $company_type" . PHP_EOL;
-    $message .= "Services Needed: $services" . PHP_EOL;
-    $message .= "Budget: $budget" . PHP_EOL . PHP_EOL;
-    $message .= "Privacy_policy_accept: $privacy_policy_accept" . PHP_EOL . PHP_EOL;
-    $message .= "Message: " . PHP_EOL . $message;
+    $mail = new PHPMailer(true);
 
-    // Implement the recommended email sending method
-    $success = mail($to, $subject, $message, "From: $email" . PHP_EOL);
+            try {
+                //Server settings
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com'; // your smtp
+                $mail->SMTPAuth = true;
+                $mail->Username = 'helloforgex@gmail.com'; // your email
+                $mail->Password = 'qciaabhuqdqyqbto'; // your pass
+                $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
 
-    if ($success) {
+        //Recipients
+        $mail->setFrom($email, $fname . ' ' . $lname);
+        // $mail->addAddress('wordpressriver@gmail.com');
+        $mail->addAddress('helloforgex@gmail.com'); // your email address
+
+
+        //Content
+        $mail->isHTML(false);
+        $mail->Subject = "Contact Form Submission from $fname $lname";
+        $mail->Body = "You have received a contact form submission from $fname $lname. Below are the details:" . PHP_EOL . PHP_EOL;
+        $mail->Body .= "First Name: $fname" . PHP_EOL;
+        $mail->Body .= "Last Name: $lname" . PHP_EOL;
+        $mail->Body .= "Email: $email" . PHP_EOL;
+        $mail->Body .= "Phone Number: $phone" . PHP_EOL;
+        $mail->Body .= "Country: $homeland" . PHP_EOL;
+        $mail->Body .= "Company Type: $company_type" . PHP_EOL;
+        $mail->Body .= "Services Needed: $services" . PHP_EOL;
+        $mail->Body .= "Budget: $budget" . PHP_EOL . PHP_EOL;
+        $mail->Body .= "Privacy_policy_accept: $privacy_policy_accept" . PHP_EOL . PHP_EOL;
+        $mail->Body .= "Message: " . PHP_EOL . $message_body;
+
+        $mail->send();
         echo '<div class="alert alert-success">';
         echo '<h3>Email Sent Successfully.</h3>';
         echo "<p>Thank you <strong>$fname</strong>, your message has been submitted to us.</p>";
         echo '</div>';
-    } else {
-        echo 'ERROR!';
+    } catch (Exception $e) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
 } else {
     http_response_code(403);
